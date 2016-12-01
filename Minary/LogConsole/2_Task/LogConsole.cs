f0a8d7577c023ce828b4d59df0641356640f240f
Ruben Unteregger <ruben.unteregger@zhdk.ch>
2016-12-01 11:52:28 +0100

    Implement log message processing thread

---
+++ Minary/LogConsole/2_Task/LogConsole.cs
@@ -0,0 +1,100 @@
+﻿namespace Minary.LogConsole.Task
+{
+  using Minary.LogConsole.DataTypes;
+  using System;
+  using System.Collections.Concurrent;
+  using System.Collections.Generic;
+  using System.Threading;
+
+
+  public class LogConsole : IObservable
+  {
+
+    #region MEMBERS
+
+    private static bool stopThreads = false;
+    private Thread processLogDataThread;
+    private ConcurrentQueue<string> logDataQueue;
+    private List<IObserver> observers;
+
+    #endregion
+
+
+    #region PUBLIC
+
+    public LogConsole()
+    {
+      this.logDataQueue = new ConcurrentQueue<string>();
+      this.processLogDataThread = new Thread(this.LogDataProcessingThread);
+      this.processLogDataThread.Start();
+      this.observers = new List<IObserver>();
+    }
+
+
+    public void AddLogMessage(string logMessage)
+    {
+      string timeStamp = DateTime.Now.ToString("yyyy-MM-dd-HH:mm:ss");
+      string realLogMessage = string.Format("{0} - {1}", timeStamp, logMessage);
+
+      this.logDataQueue.Enqueue(realLogMessage);
+    }
+
+    #endregion
+
+
+    #region PRIVATE
+
+    public void LogDataProcessingThread()
+    {
+      List<string> tmpDataQueue = new List<string>();
+      string tmpRecord = string.Empty;
+
+      while (stopThreads == false)
+      {
+        if (this.logDataQueue.IsEmpty == true)
+        {
+          Thread.Sleep(300);
+          continue;
+        }
+
+        tmpDataQueue.Clear();
+
+        // Process all records in queue
+        while (!this.logDataQueue.IsEmpty && tmpDataQueue.Count <= 3)
+        {
+          tmpRecord = string.Empty;
+          if (this.logDataQueue.TryDequeue(out tmpRecord))
+          {
+            tmpDataQueue.Add(tmpRecord);
+          }
+        }
+
+        this.Notify(tmpDataQueue);
+      }
+    }
+
+    #endregion
+
+
+    #region INTERFACE: IObservable
+
+    public void AddObserver(IObserver observer)
+    {
+      if (observer != null)
+      {
+        this.observers.Add(observer);
+      }
+    }
+
+    public void Notify(List<string> newLogRecords)
+    {
+      foreach (IObserver observer in this.observers)
+      {
+        observer.UpdateLog(newLogRecords);
+      }
+    }
+
+    #endregion
+
+  }
+}
---
+++ Minary/LogConsole/DataTypes/IObserver.cs
@@ -0,0 +1,9 @@
+﻿namespace Minary.LogConsole.DataTypes
+{
+  using System.Collections.Generic;
+
+  public interface IObserver
+  {
+    void UpdateLog(List<string> newLogMessages);
+  }
+}
