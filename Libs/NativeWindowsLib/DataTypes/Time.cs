f9e5fadbc387d77600c14ee9d5951818b42df6c1
Ruben Unteregger <ruben.unteregger@zhdk.ch>
2016-12-02 15:53:54 +0100

    Implement library containing native Windows methods bindings.

---
+++ Libs/NativeWindowsLib/DataTypes/Time.cs
@@ -0,0 +1,27 @@
+﻿namespace NativeWindowsLib.DataTypes
+{
+  using System.Runtime.InteropServices;
+
+
+  public class Time
+  {
+
+    #region DATA TYPES
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SystemTime
+    {
+      public short Year;
+      public short Month;
+      public short DayOfWeek;
+      public short Day;
+      public short Hour;
+      public short Minute;
+      public short Second;
+      public short Milliseconds;
+    }
+
+    #endregion
+
+  }
+}
