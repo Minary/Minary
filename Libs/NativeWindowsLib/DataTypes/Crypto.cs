068a5c05b6c84fd4a28311aab770ec22c9624af7
Ruben Unteregger <ruben.unteregger@zhdk.ch>
2016-12-02 15:53:54 +0100

    Implement library containing native Windows methods bindings.

---
+++ Libs/NativeWindowsLib/DataTypes/Crypto.cs
@@ -0,0 +1,52 @@
+﻿namespace NativeWindowsLib.DataTypes
+{
+  using System;
+  using System.Runtime.InteropServices;
+
+
+  public class Crypto
+  {
+
+    #region DATA TYPES
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct CryptoApiBlob
+    {
+      public int DataLength;
+      public IntPtr Data;
+
+      public CryptoApiBlob(int dataLength, IntPtr data)
+      {
+        this.DataLength = dataLength;
+        this.Data = data;
+      }
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct CryptKeyProviderInformation
+    {
+      [MarshalAs(UnmanagedType.LPWStr)]
+      public string ContainerName;
+      [MarshalAs(UnmanagedType.LPWStr)]
+      public string ProviderName;
+      public int ProviderType;
+      public int Flags;
+      public int ProviderParameterCount;
+      public IntPtr ProviderParameters; // PCRYPT_KEY_PROV_PARAM
+      public int KeySpec;
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct CRYPT_DATA_BLOB
+    {
+      [MarshalAs(UnmanagedType.U4)]
+      public uint cbData;
+      public IntPtr pbData;
+    }
+
+    #endregion
+
+  }
+}
---
+++ Libs/NativeWindowsLib/Time.cs
@@ -0,0 +1,17 @@
+﻿namespace NativeWindowsLib
+{
+  using System.Runtime.InteropServices;
+  using TimeTypes = NativeWindowsLib.DataTypes.Time;
+
+
+  public class Time
+  {
+
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool FileTimeToSystemTime(
+        [In] ref long fileTime,
+        out TimeTypes.SystemTime systemTime);
+
+  }
+}
