ef2b891561b1726f95f41128183966cafa00fa20
Ruben Unteregger <ruben.unteregger@gmail.com>
2019-04-07 20:28:13 +0200

    feat: Add basic SimpleGUI User Control

---
+++ Minary/Form/Main/1_Presentation/SimpleGUI/SimpleGuiUserControl.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Minary.Form.SimpleGUI
+{
+  partial class SimpleGuiUserControl
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.dgv_SimpleGui = new System.Windows.Forms.DataGridView();
+      ((System.ComponentModel.ISupportInitialize)(this.dgv_SimpleGui)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // dgv_SimpleGui
+      // 
+      this.dgv_SimpleGui.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dgv_SimpleGui.Location = new System.Drawing.Point(13, 17);
+      this.dgv_SimpleGui.Name = "dgv_SimpleGui";
+      this.dgv_SimpleGui.RowTemplate.Height = 28;
+      this.dgv_SimpleGui.Size = new System.Drawing.Size(240, 150);
+      this.dgv_SimpleGui.TabIndex = 0;
+      // 
+      // SimpleGuiUserControl
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.dgv_SimpleGui);
+      this.Name = "SimpleGuiUserControl";
+      this.Size = new System.Drawing.Size(1366, 830);
+      ((System.ComponentModel.ISupportInitialize)(this.dgv_SimpleGui)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.DataGridView dgv_SimpleGui;
+  }
+}
