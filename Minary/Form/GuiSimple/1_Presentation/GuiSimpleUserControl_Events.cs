ce9f758ebfc225adb233ac2cba097627dae64cda
Ruben Unteregger <ruben.unteregger@gmail.com>
2019-06-30 13:00:44 +0200

    fix: Clear SimpleGUI target list after returning from AdvancedGUI

--- Minary/Form/GuiSimple/1_Presentation/GuiSimpleUserControl_Events.cs
+++ Minary/Form/GuiSimple/1_Presentation/GuiSimpleUserControl_Events.cs
@@ -9,7 +9,6 @@
   using System;
   using System.Collections.Generic;
   using System.ComponentModel;
-  using System.Linq;
   using System.Windows.Forms;
 
 
@@ -114,6 +113,8 @@
 
         return;
       }
+
+      this.targetStringList.Clear();
 
       // Configure ARP scan object
       //this.arpScanner.Config = this.GetArpScanConfig();
